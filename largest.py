4643726207bae75959ece60e00ec463dbb627675
belamahir3 <42638518+belamahir3@users.noreply.github.com>
2018-08-23 18:11:54 +0100

    Rename larger.py to largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+num1=float(input("enter a number1:"))
+num2=float(input("enter a number2:"))
+num3=float(input("enter a number3:"))
+if num1>num2 and num1>num3:
+    print("num1  is largest")
+elif num2>num1 and num2>num3:
+    print("num2 is largest")
+else:
+    print("num3 is largest")
