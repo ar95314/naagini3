639811485fd86a6def4435a3f373f5b4777104f7
belamahir3 <42638518+belamahir3@users.noreply.github.com>
2018-08-27 11:43:49 +0100

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,11 @@
-num1=float(input("enter a number1:"))
-num2=float(input("enter a number2:"))
-num3=float(input("enter a number3:"))
+num1=int(raw_input())
+num2=int(raw_input())
+num3=int(raw_input())
 if num1>num2 and num1>num3:
-    print("num1  is largest")
+    print("num1")
 elif num2>num1 and num2>num3:
-    print("num2 is largest")
+    print("num2")
 else:
-    print("num3 is largest")
+    print("num3")
+    
+
