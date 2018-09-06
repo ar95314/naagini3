df6158acf9ee14fa8bfb3837a78890640cd4b633
belamahir3 <42638518+belamahir3@users.noreply.github.com>
2018-09-06 06:38:13 +0100

    Update 28.py

--- 28.py
+++ 28.py
@@ -1,4 +1,4 @@
-n=int(raw_input())
-list=[int(x) for x in raw_input().split()]
-for i in range(0,n):
+m=int(raw_input())
+list=[int(y) for y in raw_input().split()]
+for i in range(0,m):
     print list[i],i
